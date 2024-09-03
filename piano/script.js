dc37c62a4d0aa55116cc181a9661b27705cd5963
Kweka-nickson <nickssonpatrick@gmail.com>
2024-09-03 00:10:40 -0700

    Add files via upload

---
+++ piano/script.js
@@ -0,0 +1,44 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key"),
+VolumeSlider = document.querySelectorAll(".volume-slider input"),
+keysCheckbox = document.querySelectorAll(".keys-checkbox input");
+
+let allKeys = [],
+audio = new Audio("tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = 'tunes/${key}.wav';
+    audio.play();
+    
+
+    const clickedKey = document.querySelector('[data-key="${key}"]');
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.removed("active");
+
+    }, 150);
+}
+
+pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
+
+});
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+const pressedKey = (e) => {
+    if(allKeys.includes(e.key)) playTune(e.key);
+
+
+}
+
+keysCheckbox.addEventListener("click", showHideKeys);
+VolumeSlider.addEventListener("input", handleVolume);
+document.addEventListener("keydown", pressedKey);
